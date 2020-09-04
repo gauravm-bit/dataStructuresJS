e1bc12e439d7ab74cc442832de292aa94c170b15
gauravm-bit <gauravwrk01@gmail.com>
2020-09-04 18:41:09 +0530

    ADDED:ability to push

--- stack.js
+++ stack.js
@@ -1,5 +1,17 @@
 let Stack = function(){
     this.count = 0;
     this.storage = {};
+
+    //ADD elements to the stack 
+    this.push = (value) => {
+        this.storage[this.count] = value;
+        this.count++;
+    }
 }
+
+myStack = new Stack()
+myStack.push(1)
+myStack.push(2)
+console.log(myStack.storage);
+
 
