cad602830406bc1fd8cb7c06bf7cb7e0027a9bba
gauravm-bit <gauravwrk01@gmail.com>
2020-09-04 19:36:12 +0530

    ADDED:ability to peek

--- stack.js
+++ stack.js
@@ -17,14 +17,25 @@ let Stack = function(){
         delete this.storage[this.count]
         return result;
     }
+
+    this.peek = () => {
+        return console.log(this.storage[this.count-1]);
+    }
 }
 
 myStack = new Stack()
 myStack.push(1)
 myStack.push(2)
+myStack.push(2)
+myStack.push(2)
+myStack.push(2)
+myStack.push(2)
+
 console.log(myStack.storage);
 myStack.pop()
+myStack.pop()
 console.log(myStack.storage);
+myStack.peek()
 
 
 
