e6182bf4c743a549e70a7a97679eeb22dd905ac2
gauravm-bit <gauravwrk01@gmail.com>
2020-09-04 19:10:05 +0530

    ADDED: ability to pop

--- stack.js
+++ stack.js
@@ -7,11 +7,24 @@ let Stack = function(){
         this.storage[this.count] = value;
         this.count++;
     }
+
+    this.pop = () => {
+        if(this.count === 0){
+            return undefined
+        }
+        this.count--;
+        let result = this.storage[this.count];
+        delete this.storage[this.count]
+        return result;
+    }
 }
 
 myStack = new Stack()
 myStack.push(1)
 myStack.push(2)
 console.log(myStack.storage);
+myStack.pop()
+console.log(myStack.storage);
+
 
 
