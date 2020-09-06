01e693ff015ca28ed58793d05967b51d50df2f9f
gauravm-bit <gauravwrk01@gmail.com>
2020-09-06 12:49:31 +0530

    ADDED:ability find the index

--- linkedlist.js
+++ linkedlist.js
@@ -2,7 +2,7 @@ function LinkedList() {
     let length = 0;
     let head = null;
 
-    let Node  = function(element){
+    let Node = function (element) {
         this.element = element;
         this.next = null;
     };
@@ -17,12 +17,12 @@ function LinkedList() {
 
     this.add = (element) => {
         let node = new Node(element);
-        if(head === null){
+        if (head === null) {
             head = node;
         }
-        else{
+        else {
             let currentNode = head;
-            while(currentNode.next){
+            while (currentNode.next) {
                 currentNode = currentNode.next;
             }
             currentNode.next = node;
@@ -33,11 +33,11 @@ function LinkedList() {
     this.remove = (element) => {
         let currentNode = head;
         let previousNode;
-        if(currentNode.element === element){
+        if (currentNode.element === element) {
             head = currentNode.next;
         }
-        else{
-            while(currentNode.element !== element){
+        else {
+            while (currentNode.element !== element) {
                 previousNode = currentNode;
                 currentNode = currentNode.next;
             }
@@ -45,10 +45,32 @@ function LinkedList() {
         }
         length--;
     }
- }
+
+    this.indexOf = (element) => {
+        let currentNode = head;
+        let index = -1;
+        while (currentNode) {
+            index++;
+            if (currentNode.element === element) {
+                return index;
+            }
+            currentNode = currentNode.next;
+        }
+        return -1;
+    }
+}
 myList = new LinkedList();
 myList.add(12)
 myList.add(13)
+myList.add(13)
+myList.add(13)
+myList.add(13)
+myList.add(18)
+myList.add(13)
+myList.add(13)
+
 console.log(myList.size());
 myList.remove(13)
 console.log(myList.size());
+
+console.log(myList.indexOf(18));
